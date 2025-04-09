6d60df965490c2f3b664becb2d1b0a6421743032
MahesaFarih <mahesafarih7@gmail.com>
2025-04-09 10:55:17 +0700

    Initial Commit

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/UdehnihhApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdehnihhApplication.class, args);
+    }
+
+}
