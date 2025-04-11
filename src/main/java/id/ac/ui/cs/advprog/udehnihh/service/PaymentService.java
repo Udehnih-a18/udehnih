2e1101f333d72c0dc713608619e1b4c4bac84103
alicfamahdi <alicfamahdi@gmail.com>
2025-04-11 19:20:03 +0700

    feat: [RED] make PaymentService implementation

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/service/PaymentService.java
@@ -1,4 +1,18 @@
 @Service
 public class PaymentService {
+    @Autowired
+    private TransactionRepository repo;
 
+    @Autowired
+    private PaymentStrategyFactory strategyFactory;
+
+    public Transaction createTransaction(Transaction tx, PaymentRequest request) {
+        PaymentStrategy strategy = strategyFactory.getStrategy(tx.getMethod());
+        strategy.pay(tx, request);
+        return repo.save(tx);
+    }
+
+    public List<Transaction> getTransactionsForStudent(UUID studentId) {
+        return repo.findByStudentId(studentId);
+    }
 }
