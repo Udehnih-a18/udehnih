12b2b335b3709cc03a3cb141d55afd07d9f9047e
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2025-04-11 17:48:02 +0700

    [RED] add Review model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/model/Review.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihh.model;
+
+public class Review {
+    String id;
+    String studentId;
+    String courseId;
+    int rating;
+    String review;
+
+    public Review(String id, String studentId, String courseId, int rating) {}
+
+    public Review(String id, String studentId, String courseId, int rating, String review) {}
+}
