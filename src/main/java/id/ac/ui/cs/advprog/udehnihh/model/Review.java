96bf64b4a982fac966a7ee75c3617fd2d96ce303
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2025-04-11 17:59:57 +0700

    [GREEN] implement Review model

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/model/Review.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/model/Review.java
@@ -7,7 +7,39 @@ public class Review {
     int rating;
     String review;
 
-    public Review(String id, String studentId, String courseId, int rating) {}
+    public Review(String id, String studentId, String courseId, int rating) {
+        this.id = id;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.rating = rating;
 
-    public Review(String id, String studentId, String courseId, int rating, String review) {}
+        if ((1 <= rating) && (rating <= 5)) {
+            this.rating = rating;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Review(String id, String studentId, String courseId, int rating, String review) {
+        this.id = id;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.rating = rating;
+        this.review = review;
+
+        if ((1 <= rating) && (rating <= 5)) {
+            this.rating = rating;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+
+        if (review.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.review = review;
+        }
+    }
 }
