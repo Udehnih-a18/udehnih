82869f14a93bf8164b4a6beedb68ab4168bd80d3
zufarra <zufarromli@gmail.com>
2025-04-11 17:22:48 +0700

    [RED] Add ReportRepository skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/repository/ReportRepository.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.udehnihh.repository;
+
+import id.ac.ui.cs.advprog.udehnihh.model.Report;
+import java.util.List;
+
+public class ReportRepository {
+
+    public void create(Report report) {
+        // TODO: Implement method
+    }
+
+    public List<Report> findAll() {
+        // TODO: Implement method
+        return null;
+    }
+
+    public Report findById(String id) {
+        // TODO: Implement method
+        return null;
+    }
+
+    public void update(String id, Report updatedReport) {
+        // TODO: Implement method
+    }
+
+    public void delete(String id) {
+        // TODO: Implement method
+    }
+}
