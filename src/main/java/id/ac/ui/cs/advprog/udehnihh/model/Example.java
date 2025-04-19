788cfc22428f2b8dd1b5fd0f5783cdfe2c62541c
zufarra <zufarromli@gmail.com>
2025-04-19 22:44:46 +0700

    ci-cd: add dummy model and dummy model test for testing ci-cd configuration.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/model/Example.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.udehnihh.model;
+
+public class Example {
+    int angka;
+
+    public Example(int angka) {
+        this.angka = angka;
+    }
+    public int getAngka() {
+        return angka;
+    }
+}
