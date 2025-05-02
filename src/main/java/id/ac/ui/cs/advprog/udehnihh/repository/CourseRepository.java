f66019fc208a0b21313e26e333c3af25fb923a0e
Madeline <mdlnecg@gmail.com>
2025-05-02 19:48:37 +0700

    [FEAT] Add Course, Section, and Article Request

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/repository/CourseRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/repository/CourseRepository.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.udehnihh.repository;
 
-public interface CourseRepository {
-    List<Course> findAll();
-    Optional<Course> findById(Long id);
-    List<Course> findByName(String name);
-    List<Course> findByTutor(User tutor);
-    List<Course> findByPriceRange(Double minPrice, Double maxPrice);
-    Course save(Course course);
+import id.ac.ui.cs.advprog.udehnihh.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+    List<Course> findByTutorId(UUID tutorId);
 }
