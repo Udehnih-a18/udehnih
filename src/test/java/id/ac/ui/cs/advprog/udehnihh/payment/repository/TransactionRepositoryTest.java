0c40e6e98bcbbbfa6c7f472889ebb309e043fe77
alicfamahdi <alicfamahdi@gmail.com>
2025-05-02 20:44:22 +0700

    [RED] add tests for models n repos

---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihh/payment/repository/TransactionRepositoryTest.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.udehnihh.payment.repository;
+
+import id.ac.ui.cs.advprog.udehnihh.payment.model.Refund;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionRepositoryTest {
+    @BeforeEach
+    public void setUp() {
+        List<Refund> refunds = new ArrayList<Refund>();
+    }
+
+    @Test
+    public void createTest() {
+
+    }
+}
