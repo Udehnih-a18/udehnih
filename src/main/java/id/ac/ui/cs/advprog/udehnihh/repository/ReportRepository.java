c7bf180e90d5199b418283ef31e6a9b0668ec989
zufarra <zufarromli@gmail.com>
2025-05-02 15:29:06 +0700

    [REFACTOR] refactor code in ReportRepository.java.

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/repository/ReportRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/repository/ReportRepository.java
@@ -1,28 +1,49 @@
 package id.ac.ui.cs.advprog.udehnihh.repository;
 
 import id.ac.ui.cs.advprog.udehnihh.model.Report;
+import org.springframework.stereotype.Repository;
+
 import java.util.*;
 
+@Repository
 public class ReportRepository {
-    private final Map<String, Report> reports = new HashMap<>();
+    private List<Report> reportData = new ArrayList<>();
 
-    public void create(Report report) {
-        reports.put(report.getIdReport(), report);
+    public Report save(Report report) {
+        int i = 0;
+        for (Report savedReport : reportData) {
+            if (savedReport.getIdReport().equals(report.getIdReport())) {
+                reportData.remove(i);
+                reportData.add(i, report);
+                return report;
+            }
+            i++;
+        }
+        reportData.add(report);
+        return report;
     }
 
-    public List<Report> findAll() {
-        return new ArrayList<>(reports.values());
+    public List<Report> findAllByAuthor(String author) {
+        List<Report> result = new ArrayList<>();
+        for (Report report : reportData) {
+            if (report.getAuthor().equals(author)) {
+                result.add(report);
+            }
+        }
+        return result;
     }
 
     public Report findById(String id) {
-        return reports.get(id);
-    }
-
-    public void update(String id, Report updatedReport) {
-        reports.put(id, updatedReport);
+        for (Report report : reportData) {
+            if (report.getIdReport().equals(id)) {
+                return report;
+            }
+        }
+        return null;
     }
 
     public void delete(String id) {
-        reports.remove(id);
+        Report report = findById(id);
+        reportData.remove(report);
     }
 }
