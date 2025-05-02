81f0f7a717963a3299ab882a0dc3aa2cf63c8fcf
Madeline <mdlnecg@gmail.com>
2025-05-02 10:59:26 +0700

    [REFACTOR] Modify User Model

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/authentication/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/authentication/model/User.java
@@ -6,6 +6,8 @@ import lombok.*;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 @Getter
@@ -18,20 +20,32 @@ import org.hibernate.annotations.CreationTimestamp;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @Column(name = "full_name", nullable = false)
     private String fullName;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
     @Builder.Default
     @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
     private Role role = Role.STUDENT;
+
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @CreationTimestamp
     private LocalDateTime registrationDate;
 
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "birth_date")
     private String birthDate;
 }
