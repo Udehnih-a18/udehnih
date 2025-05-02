478e01c130dfdc7ad0d6e438c9ec3b83738d3f38
alicfamahdi <alicfamahdi@gmail.com>
2025-05-02 20:19:02 +0700

    [RED] add implementation for Refund

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/payment/enums/RefundStatus.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.udehnihh.payment.enums;
+
+public enum RefundStatus {
+    ACCEPTED,
+    REJECTED,
+    ON_HOLD
+}
--- src/main/java/id/ac/ui/cs/advprog/udehnihh/payment/model/Refund.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/payment/model/Refund.java
@@ -1,6 +1,19 @@
 package id.ac.ui.cs.advprog.udehnihh.payment.model;
 
+import id.ac.ui.cs.advprog.udehnihh.payment.enums.RefundStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Refund {
     private Transaction transaction;
     private String reasonForRefund;
+    private RefundStatus status;
+
+    public Refund(Transaction transaction, String reasonForRefund) {
+        this.transaction = transaction;
+        this.reasonForRefund = reasonForRefund;
+        this.status = RefundStatus.ON_HOLD;
+    }
 }
