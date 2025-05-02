e0e2e1f93cbfd12d345f66675c4da0467f0a1ada
zufarra <zufarromli@gmail.com>
2025-05-02 15:34:08 +0700

    [REFACTOR] Implement ReportServiceImpl.java.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/service/ReportServiceImpl.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.advprog.udehnihh.service;
+
+import id.ac.ui.cs.advprog.udehnihh.model.Report;
+import id.ac.ui.cs.advprog.udehnihh.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Override
+    public Report createReport(Report report) {
+        if (reportRepository.findById(report.getIdReport()) == null){
+            reportRepository.save(report);
+            return report;
+        }
+        return null;
+    }
+
+    @Override
+    public Report getReportById(String id) {
+        return reportRepository.findById(id);
+    }
+
+    @Override
+    public Report updateReport(String reportId, String title, String description) {
+        Report report = reportRepository.findById(reportId);
+        if (report != null) {
+            report.setTitle(title);
+            report.setDescription(description);
+            reportRepository.save(report);
+            return report;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public void deleteReport(String id) {
+        reportRepository.delete(id);
+    }
+}
