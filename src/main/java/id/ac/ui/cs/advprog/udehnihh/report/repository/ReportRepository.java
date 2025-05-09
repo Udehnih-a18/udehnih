749bd1026dfd1e773fce7702ece457a666c5c073
zufarra <zufarromli@gmail.com>
2025-05-09 19:35:05 +0700

    [REFACTOR] Change Report Repository to interface.

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/report/repository/ReportRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/report/repository/ReportRepository.java
@@ -1,53 +1,11 @@
 package id.ac.ui.cs.advprog.udehnihh.report.repository;
 
 import id.ac.ui.cs.advprog.udehnihh.report.model.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.List;
 
 @Repository
-public class ReportRepository {
-    private List<Report> reportData = new ArrayList<>();
-
-    public Report save(Report report) {
-        int i = 0;
-        for (Report savedReport : reportData) {
-            if (savedReport.getIdReport().equals(report.getIdReport())) {
-                reportData.remove(i);
-                reportData.add(i, report);
-                return report;
-            }
-            i++;
-        }
-        reportData.add(report);
-        return report;
-    }
-
-    public List<Report> findAllByAuthor(String author) {
-        List<Report> result = new ArrayList<>();
-        for (Report report : reportData) {
-            if (report.getAuthor().equals(author)) {
-                result.add(report);
-            }
-        }
-        return result;
-    }
-
-    public List<Report> findAll() {
-        return reportData;
-    }
-
-    public Report findById(String id) {
-        for (Report report : reportData) {
-            if (report.getIdReport().equals(id)) {
-                return report;
-            }
-        }
-        return null;
-    }
-
-    public void delete(String id) {
-        Report report = findById(id);
-        reportData.remove(report);
-    }
+public interface ReportRepository extends JpaRepository<Report, String> {
 }
