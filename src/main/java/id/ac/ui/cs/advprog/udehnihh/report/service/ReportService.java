760c5c5d7533660decf9a1ae51ee5595e05f92d6
zufarra <zufarromli@gmail.com>
2025-05-09 19:37:27 +0700

    [REFACTOR] Change some attribute parameter method in Report Service.

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/report/service/ReportService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/report/service/ReportService.java
@@ -1,16 +1,13 @@
 package id.ac.ui.cs.advprog.udehnihh.report.service;
 
 import id.ac.ui.cs.advprog.udehnihh.report.model.Report;
-
 import java.util.List;
 
 public interface ReportService {
-    public Report createReport(Report report);
-    public Report updateReport(String reportId, String title, String description);
-    public void deleteReport(String id);
-    public Report getReportById(String id);
-    public List<Report> getReportsByAuthor(String authorId);
-    public List<Report> getAllReports();
-
-
+    Report getReportById(String id);
+    List<Report> getAllReports();
+    List<Report> getReportsByAuthor(String author);
+    Report createReport(Report report);
+    Report updateReport(String reportId, String username, String title, String description);
+    void deleteReport(String id, String username);
 }
