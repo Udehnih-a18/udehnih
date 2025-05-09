36d62947a33c446f0f5eb2930205f8b58bb19af6
Madeline <mdlnecg@gmail.com>
2025-05-09 20:01:24 +0700

    [GREEN] Add TutorApplication model

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/course/model/TutorApplication.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.udehnihh.course.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import id.ac.ui.cs.advprog.udehnihh.authentication.model.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tutor_applications")
+public class TutorApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @Column(name = "applicant", nullable = false)
+    private User applicant;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ApplicationStatus status = ApplicationStatus.PENDING;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public enum ApplicationStatus {
+        PENDING,
+        ACCEPTED,
+        DENIED
+    }
+}
+
