c36dfc4969931095af4834d32c25311137cca6f8
MahesaFarih <mahesafarih7@gmail.com>
2025-05-21 19:20:47 +0700

    [REFACTOR] add course delete/accept function

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/course/service/CourseCreationService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/course/service/CourseCreationService.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import id.ac.ui.cs.advprog.udehnihh.course.dto.CourseSummaryDto;
+import id.ac.ui.cs.advprog.udehnihh.course.mapper.CourseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import id.ac.ui.cs.advprog.udehnihh.course.enums.CourseStatus;
 import id.ac.ui.cs.advprog.udehnihh.authentication.model.User;
 import id.ac.ui.cs.advprog.udehnihh.authentication.repository.UserRepository;
 import id.ac.ui.cs.advprog.udehnihh.course.dto.CourseRequest;
 import id.ac.ui.cs.advprog.udehnihh.course.model.Course;
+
 import id.ac.ui.cs.advprog.udehnihh.course.repository.CourseCreationRepository;
 import jakarta.transaction.Transactional;
 
@@ -49,6 +52,41 @@ public class CourseCreationService {
         course.setTutor(tutor.get());
 
         return courseCreationRepository.save(course);
+    }
+
+    @Transactional
+    public Course approveCourse(UUID courseId) {
+        Course course = courseCreationRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        if (course.getStatus() != CourseStatus.PENDING_APPROVAL) {
+            throw new IllegalStateException("Course is not pending approval");
+        }
+
+        course.setStatus(CourseStatus.PUBLISHED);
+        return courseCreationRepository.save(course);
+
+    }
+
+    @Transactional
+    public Course rejectCourse(UUID courseId) {
+        Course course = courseCreationRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        if (course.getStatus() != CourseStatus.PENDING_APPROVAL) {
+            throw new IllegalStateException("Course is not pending approval");
+        }
+
+        course.setStatus(CourseStatus.REJECTED);
+        return courseCreationRepository.save(course);
+
+    }
+
+    public List<CourseSummaryDto> getAllCoursesDTO() {
+        List<Course> courses = courseCreationRepository.findAll();
+        return courses.stream()
+                .map(CourseMapper::toSummary)
+                .toList();
     }
 
     public void deleteCourse(UUID id) {
