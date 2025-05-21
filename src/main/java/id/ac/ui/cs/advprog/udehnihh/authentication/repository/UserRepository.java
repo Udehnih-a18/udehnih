51fbfea6d15176f02bcc077bbbe42cc002644954
MahesaFarih <mahesafarih7@gmail.com>
2025-05-21 19:02:14 +0700

    [REFACTOR] add find by role for Course Repository

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/authentication/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/authentication/repository/UserRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import id.ac.ui.cs.advprog.udehnihh.authentication.model.User;
+import id.ac.ui.cs.advprog.udehnihh.authentication.enums.Role;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByRole(Role role);
 }
