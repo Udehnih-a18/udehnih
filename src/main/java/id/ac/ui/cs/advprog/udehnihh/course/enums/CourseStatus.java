041585d8b4236a3e67a3d6281e38849df150d353
MahesaFarih <mahesafarih7@gmail.com>
2025-05-21 18:41:45 +0700

    [REFACTOR] add CourseStatus enum for Course

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/course/enums/CourseStatus.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.udehnihh.course.enums;
+
+// Add to Course.java
+public enum CourseStatus {
+    PENDING_APPROVAL,
+    PUBLISHED,
+    REJECTED
+}
+
+// Then add this field to the Course class:
