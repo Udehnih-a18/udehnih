def6f2f8e4228e491f6c7a3c23f9e7f4a2511b92
RelAthalla <farrelathalla0910@gmail.com>
2025-05-23 20:50:14 +0700

    [Feat] using asyncronous for creating enrollment

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/UdehnihhApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/UdehnihhApplication.java
@@ -2,8 +2,10 @@ package id.ac.ui.cs.advprog.udehnihh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class UdehnihhApplication {
 
     public static void main(String[] args) {
