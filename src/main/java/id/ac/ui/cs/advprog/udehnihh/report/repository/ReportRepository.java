efa2c2fe6f5f3ef4c3a2b52ddb218d7340abd776
zufarra <zufarromli@gmail.com>
2025-05-23 09:21:22 +0700

    [REFACTOR]: Modify param method on Report Repository.

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/report/repository/ReportRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/report/repository/ReportRepository.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.udehnihh.report.repository;
 
 import id.ac.ui.cs.advprog.udehnihh.report.model.Report;
+import id.ac.ui.cs.advprog.udehnihh.authentication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, String> {
-    List<Report> findAllByAuthor(String author);
+    List<Report> findAllByCreatedBy(User user);
 }
