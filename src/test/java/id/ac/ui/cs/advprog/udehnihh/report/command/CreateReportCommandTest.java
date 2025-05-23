001f1b14950f6a525c138703dbf528c9208b1be1
zufarra <zufarromli@gmail.com>
2025-05-23 09:22:30 +0700

    [RED]: Add tests for Create Report Command.

---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihh/report/command/CreateReportCommandTest.java
@@ -0,0 +1,41 @@
+package id.ac.ui.cs.advprog.udehnihh.report.command;
+
+import id.ac.ui.cs.advprog.udehnihh.report.command.CreateReportCommand;
+import id.ac.ui.cs.advprog.udehnihh.report.model.Report;
+import id.ac.ui.cs.advprog.udehnihh.report.repository.ReportRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class CreateReportCommandTest {
+
+    private ReportRepository repository;
+    private Report report;
+
+    @BeforeEach
+    void setUp() {
+        repository = mock(ReportRepository.class);
+        report = Report.builder()
+                .idReport(UUID.randomUUID().toString())
+                .title("Create Title")
+                .description("Create Description")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+
+    @Test
+    void testExecuteCreatesReportSuccessfully() {
+        when(repository.save(report)).thenReturn(report);
+        CreateReportCommand command = new CreateReportCommand(report, repository);
+        Report result = command.execute();
+
+        assertEquals(report.getTitle(), result.getTitle());
+        verify(repository, times(1)).save(report);
+    }
+}
