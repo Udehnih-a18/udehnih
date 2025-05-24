929df1d7dd4b1c37889a2c77742076385fd55f20
zufarra <zufarromli@gmail.com>
2025-05-24 14:20:47 +0700

    Add security config to prevent form login from spring.

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/authentication/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.udehnihh.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf().disable()
+                .formLogin().disable();
+        return http.build();
+    }
+}
+
