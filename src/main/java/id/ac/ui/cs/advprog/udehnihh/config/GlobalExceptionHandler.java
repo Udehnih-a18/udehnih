3598874d11258f63b559d4e54e6f4f64df7f0c11
zufarra <zufarromli@gmail.com>
2025-05-25 09:35:02 +0700

    [REFACTOR]: Fix maintainability issue in global exception handler.

--- src/main/java/id/ac/ui/cs/advprog/udehnihh/config/GlobalExceptionHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihh/config/GlobalExceptionHandler.java
@@ -10,12 +10,12 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.internalServerError().body(Map.of("message", ex.getMessage()));
     }
 
     @ExceptionHandler(org.springframework.security.access.AccessDeniedException.class)
-    public ResponseEntity<?> handleAccessDeniedException(Exception ex) {
+    public ResponseEntity<Map<String, String>> handleAccessDeniedException(Exception ex) {
         return ResponseEntity.status(403).body(Map.of("message", ex.getMessage()));
     }
 }
